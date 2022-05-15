ad2a9e295e9a54c348a6180ca6628e723bef23f0
lpmaltina <lyudmilamaltina@gmail.com>
2022-05-16 00:32:48 +0300

    fix some bugs
    If the text field is empty, now the program prints 'Empty field'.
    If the text has less than 5 lemmas, now the program prints 'The text must have at least 5 words'.
    If the text is written in the language, different from English, now the program prints 'The text must be in English'.

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import joblib
 import json
 import os
 import spacy
+from spacy_langdetect import LanguageDetector
 import streamlit as st
 from text import Text
 
@@ -24,11 +25,22 @@ class PassthroughTransformer(BaseEstimator, TransformerMixin):
 class MyTfidfVectorizer(TfidfVectorizer):
     def fit_transform(self, raw_documents, y=None):
         X = TfidfVectorizer.fit_transform(self, raw_documents, y=None)
-        return X / X.sum(axis=1)
+        try:
+            return X / X.sum(axis=1)
+        except:
+            return X
 
     def transform(self, raw_documents):
         X = TfidfVectorizer.transform(self, raw_documents)
-        return X / X.sum(axis=1)
+        try:
+            return X / X.sum(axis=1)
+        except:
+            return X
+
+
+@spacy.language.Language.factory('language_detector')
+def language_detector(nlp, name):
+    return LanguageDetector()
 
 
 @st.cache(allow_output_mutation=True)
@@ -40,6 +52,7 @@ def load_models_and_data(
     except IOError:
         spacy.cli.download("en_core_web_lg")
         nlp = spacy.load("en_core_web_lg")
+    nlp.add_pipe('language_detector', last=True)
     with open(abstract_nouns_file, "rb") as f:
         abstract = pickle.load(f)
     with open(concrete_nouns_file, "rb") as f:
@@ -50,6 +63,21 @@ def load_models_and_data(
     return nlp, abstract, concrete, word2level, pipeline
 
 
+def classify(doc, pipeline, abstract, concrete, word2level):
+    try:
+        text_obj = Text(doc)
+    except ValueError:
+        st.write("The text must have at least 5 words!")
+    else:
+        if doc._.language['language'] == "en":
+            text_df = text_obj.create_df(abstract, concrete, word2level)
+            st.text(f"{pipeline.predict(text_df)[0]}")
+        else:
+            st.text("The text must be in English!")
+
+
+st.title("Text Complexity for English")
+text = st.text_area("Paste your text below", height=300)
 nlp, abstract, concrete, word2level, pipeline = load_models_and_data(
     abstract_nouns_file=os.path.join("serialized", "abstract_nouns.pkl"),
     concrete_nouns_file=os.path.join("serialized", "concrete_nouns.pkl"),
@@ -57,10 +85,9 @@ nlp, abstract, concrete, word2level, pipeline = load_models_and_data(
     pipeline_file=os.path.join("serialized", "all-lr.joblib"),
 )
 
-st.title("Text Complexity for English")
-text = st.text_area("Paste your text below", height=300)
-
 if st.button("Get Level"):
-    tokens = nlp(text)
-    text_df = Text(tokens).create_df(abstract, concrete, word2level)
-    st.text(f"{pipeline.predict(text_df)[0]}")
+    if not text:
+        st.text("Empty field!")
+    else:
+        doc = nlp(text)
+        classify(doc, pipeline, abstract, concrete, word2level)
